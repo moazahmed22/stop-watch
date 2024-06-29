152cded69ef6821d557818025567621c05e3376c
Moaz Ahmed <nabelnadel21@gmail.com>
2024-06-29 19:20:16 +0300

    add comments to index.js

--- index.js
+++ index.js
@@ -4,23 +4,26 @@ let startedTime = 0;
 let elapsedTime = 0;
 let currentTime = 0;
 let isRunning = false;
-
+/*the stopwatch works by calculating the current time and then subtract 
+the startedTime from it, so we get the elapsedTime to display it*/
 function start() {
     if (!isRunning) {
+        //calculating the startedTime (*the only part that i couldn't understand*)
         startedTime = Date.now() - elapsedTime;
         timer = setInterval(update, 10);
         isRunning = true;
     }
 }
 
-
+//if the stopWatch isRunning then we clear the interval and we set isRunning to false
 function stop() {
     if (isRunning) {
         clearInterval(timer);
         isRunning = false;
     }
 }
-
+/*update works every 10ms it get the current time and subtract 
+the startedTime from it, then calculates the HH:MM:SS:MS and displays it*/
 function update() {
     elapsedTime = Date.now() - startedTime;
     let hours = Math.floor(elapsedTime / (1000 * 3600));
